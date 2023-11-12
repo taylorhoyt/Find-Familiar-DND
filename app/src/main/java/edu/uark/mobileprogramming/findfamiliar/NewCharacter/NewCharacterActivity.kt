a5e1dc787e2d251b85a02b4f7602797fbac816c9
teaganrm <morrisonrteagan@gmail.com>
2023-11-12 14:22:42 -0600

    - added fragments to new char activity

--- app/src/main/java/edu/uark/mobileprogramming/findfamiliar/NewCharacter/NewCharacterActivity.kt
+++ app/src/main/java/edu/uark/mobileprogramming/findfamiliar/NewCharacter/NewCharacterActivity.kt
@@ -1,25 +1,45 @@
 package edu.uark.mobileprogramming.findfamiliar.NewCharacter
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import edu.uark.mobileprogramming.findfamiliar.R
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class NewCharacterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_character)
-        val returnHome = findViewById<Button>(R.id.returnHome)
-        returnHome.setOnClickListener {
-            CoroutineScope(SupervisorJob()).launch {
-                Log.d("New Character Activity", "Button Clicked")
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.person -> replaceFragment(CharacterInfoFragment())
+                R.id.weapon -> replaceFragment(WeaponInfoFragment())
+                R.id.skills -> replaceFragment(SkillsInfoFragment())
+                R.id.ability -> replaceFragment(AbilityInfoFragment())
+                R.id.savingthrow -> replaceFragment(SavingThrowInfoFragment())
             }
+            true
+        }
+
+        // Initialize with the default fragment
+        if (savedInstanceState == null) {
+            replaceFragment(CharacterInfoFragment())
+        }
+
+        val fab = findViewById<FloatingActionButton>(R.id.homeBtn)
+        fab.setOnClickListener {
             setResult(RESULT_OK)
             finish()
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.new_fragment_container, fragment)
+            .commit()
     }
 }
