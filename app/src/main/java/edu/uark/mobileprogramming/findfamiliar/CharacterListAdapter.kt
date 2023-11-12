3e79844617dcc2c38c6493e4ed396bcc4be325af
teaganrm <morrisonrteagan@gmail.com>
2023-11-12 12:04:24 -0600

    - fixed merge issues

--- app/src/main/java/edu/uark/mobileprogramming/findfamiliar/CharacterListAdapter.kt
+++ app/src/main/java/edu/uark/mobileprogramming/findfamiliar/CharacterListAdapter.kt
@@ -19,7 +19,7 @@ class CharacterListAdapter(val characterClicked:(characters:Characters)->Unit): 
     //Displays data and content
     override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
         val current = getItem(position)
-        holder.bind(current.characterName, current.characterClass, current.characterLevel)
+        holder.bind(current.name, current.className, current.level)
         holder.itemView.tag= current
         holder.itemView.setOnClickListener{
             characterClicked(holder.itemView.tag as Characters)
@@ -51,7 +51,7 @@ class CharacterListAdapter(val characterClicked:(characters:Characters)->Unit): 
             return oldItem === newItem
         }
         override fun areContentsTheSame(oldItem: Characters, newItem: Characters): Boolean {
-            return oldItem.characterName == newItem.characterName
+            return oldItem.name == newItem.name
         }
     }
 }
