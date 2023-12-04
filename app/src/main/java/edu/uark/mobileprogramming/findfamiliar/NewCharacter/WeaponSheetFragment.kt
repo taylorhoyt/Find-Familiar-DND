5411440591eca4a6986c2a30554b02439407a070
teaganrm <112922785+teaganrm@users.noreply.github.com>
2023-12-04 09:51:39 -0600

    - added spinner items in add weapon section

--- app/src/main/java/edu/uark/mobileprogramming/findfamiliar/NewCharacter/WeaponSheetFragment.kt
+++ app/src/main/java/edu/uark/mobileprogramming/findfamiliar/NewCharacter/WeaponSheetFragment.kt
@@ -8,7 +8,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
