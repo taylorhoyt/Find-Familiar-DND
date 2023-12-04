6fa50f8d2f46d09c494cfdcb22bc7cf409b873bf
teaganrm <112922785+teaganrm@users.noreply.github.com>
2023-12-04 11:19:12 -0600

    - added padding on some screens - fixed button view for Pixel 3a - changed result text

--- app/src/main/java/edu/uark/mobileprogramming/findfamiliar/NewCharacter/SkillsInfoFragment.kt
+++ app/src/main/java/edu/uark/mobileprogramming/findfamiliar/NewCharacter/SkillsInfoFragment.kt
@@ -88,7 +88,7 @@ class SkillsInfoFragment : Fragment() {
     private fun handleButtonClick(modifier: Int, resultDisplay: TextView) {
         var total = Random.nextInt(1,20)
         total+=modifier
-        resultDisplay.text = "Result: "+total
+        resultDisplay.text = total.toString()
     }
 
     companion object {
